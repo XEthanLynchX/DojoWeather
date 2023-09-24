6dd2dc58aad86d3f549801b2a18bb338f2d88815
Ethan Lynch <ethanlynch72@gmail.com>
2023-09-23 23:50:35 -0400

    dojo weather

---
+++ script.js
@@ -0,0 +1,15 @@
+function cel(temp) {
+    return Math.round(temp * 9/5 + 23)
+}
+    
+function far(temp){
+    return Math.round((temp - 32) * 5/9)
+}
+
+function degreeChange(temp){
+    
+}
+
+function cookie(element){
+    alert
+}
